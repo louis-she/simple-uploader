19725e02bcb5b2eab5d580aeade956faf72e368a
chenglu <chenglu.she@gmail.com>
2023-05-02 00:39:06 +0800

    change ver and release

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -23,7 +23,7 @@ def read_requirements(path):
 
 setup(
     name="simple_uploader",
-    version="0.0.3",
+    version="0.0.4",
     description="Simple uploader python client",
     url="https://github.com/louis-she/simple-uploader",
     long_description=read("../../README.md"),
